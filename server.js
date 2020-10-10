327b83c83e38f9a4b6a9c43e5bfc6c11d9429adb
Eric Folenta <ericfolenta@gmail.com>
2020-10-09 18:58:03 -0700

    added routes

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ var path = require("path");
 var fs = require("fs");
 
 var app = express();
-var PORT = process.env.PORT ||3000;
+var PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 app.use(express.static(__dirname));
 
@@ -14,4 +16,73 @@ app.use(express.static(__dirname));
 // =============================================================
 app.listen(PORT, function() {
     console.log(`App listening on http://localhost:${PORT}`);
-  });
+});
+
+fs.readFile("./db/db.json", "utf8", function(err, data) {
+    if (err) {
+        throw (err);
+    }
+    var notes = JSON.parse(data);
+
+    function updateNotes() {
+        fs.writeFile("./db/db.json", JSON.stringify(notes, "\t"), function(error) {
+            if (error) {
+                throw error;
+            }
+            return true;
+        });
+    }
+
+    //API ROUTES
+    // Populate the left column
+    app.get("/api/notes", function(req, res) {
+        res.json(notes);
+    });
+
+
+    // sets up post route
+    app.post("/api/notes", function(req, res) {
+        let newNote = req.body;
+        let id = (notes.length).toString();
+        newNote.id = id
+        notes.push(newNote);
+        res.json(newNote);
+        updateNotes();
+        return console.log("Added new note: " + newNote.title);
+    });
+
+    // Deleting a note with a specific ID
+
+    app.delete("/api/notes/:id", function(req, res) {
+
+        // splice function returns removed item
+        notes.splice(req.params.id, 1);
+        var chosen = req.params.id;
+        console.log(chosen);
+        for (var i = 0; i < notes.length; i++) {
+            if (chosen === notes[i].routeName) {
+                console.log("Deleted note" + notes[i].title);
+                return res.json(notes[i]);
+            }
+        }
+        return res.json(false);
+
+    });
+
+    // Get notes based on ID
+    app.get("/api/notes/:id", function(req, res) {
+        res.json(notes(req.params.id));
+    });
+
+
+    // View the routes
+    app.get("/notes", function(req, res) {
+        res.sendFile(path.join(__dirname, "./public/notes.html"));
+    });
+
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "./public/index.html"));
+    });
+
+
+});
